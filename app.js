94ad1751f0ada8025c07d88b744bca3ffd1a1e2c
KuldeepJ <jspeedluk@github.com>
2019-11-18 14:33:08 +0530

    setting up push to heroku

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require('dotenv').config()
+// connect Mongoose to your DB
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nodeappkuldeep');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

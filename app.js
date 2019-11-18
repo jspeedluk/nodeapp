7c0abd53e85684415b79dc8d4bf6bea970ff2cee
KuldeepJ <jspeedluk@github.com>
2019-11-18 15:06:32 +0530

    add cool face api

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// const PORT = process.env.PORT || 3000;
+const cool = require('cool-ascii-faces')
 require('dotenv').config()
 // connect Mongoose to your DB
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nodeappkuldeep');
+// var mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nodeappkuldeep');
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/dhankhar', (req, res) => res.send('Hello dhankhar!'))
+app.get('/cool', (req, res) => res.send(cool()))
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
